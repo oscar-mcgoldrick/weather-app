a22a4e053170b65ceefa873e694d7619a2f2b708
Don Smith <don@antiantonym.com>
2019-01-29 11:26:34 +1300

    refactor test setup

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,15 +1,10 @@
-/* global test expect */
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import {shallow} from 'enzyme'
 
 import App from '../client/components/App'
-
-Enzyme.configure({adapter: new Adapter()})
 
 test('<App />', () => {
   const expected = 'React development has begun!'
   const wrapper = shallow(<App />)
   expect(wrapper.text()).toBe(expected)
 })
-
