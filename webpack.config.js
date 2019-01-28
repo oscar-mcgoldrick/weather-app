f57b073a0dbd26f3bab839b47281928a9856b8a3
Don Smith <don@antiantonym.com>
2019-01-29 11:08:02 +1300

    update webpack dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,9 @@ module.exports = {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
+  mode: 'development',
   module: {
-    loaders: [{
+    rules: [{
       test: /\.jsx?$/,
       loader: 'babel-loader',
       exclude: /node_modules/
@@ -18,4 +19,3 @@ module.exports = {
   },
   devtool: 'source-map'
 }
-
