77cf7885897ec23c51d7d8a073769c6e98f4a2e4
Ysabel Guiang <ysabelregino@yahoo.com>
2022-06-14 10:26:49 +1200

    add alt tag for img

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div className="title">
-        <img src="/images/color_earth.gif" />
+        <img src="/images/color_earth.gif" alt="A coloured globe of the earth spinning around on its axis"/>
         <h1>Navigating the worldwide routes</h1>
       </div>
       {/* This 'main' div is only for styling (so we can use flexbox) */}
