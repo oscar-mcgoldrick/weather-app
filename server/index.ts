59e00a97998d79ed1d9ad047769ba345b310a62d
Gerard Paapu <gerard@gerardpaapu.com>
2023-04-17 13:58:16 +1200

    feat: converted worlwide-routing to vite

--- server/index.ts
+++
@@ -1,8 +0,0 @@
-import server from './server'
-
-const PORT = process.env.PORT || 3000
-
-server.listen(PORT, function () {
-  // eslint-disable-next-line no-console
-  console.log('Listening on port', PORT)
-})
--- server/server.ts
+++
@@ -1,8 +0,0 @@
-import { join } from 'node:path'
-import express from 'express'
-
-const server = express()
-
-server.use(express.static(join(__dirname, 'public')))
-
-export default server
